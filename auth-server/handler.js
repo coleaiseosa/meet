1c1ad48e23707d1e4a562be2ab3ed2c4147832a3
coleaiseosa <coleaiseosa@gmail.com>
2023-01-02 07:52:32 -0800

    created test-auth-server

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -7,6 +7,11 @@ const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 // Credentials are those values required to get access to your calendar
 
+console.log(process.env.CLIENT_ID);
+console.log(process.env.PROJECT_ID);
+console.log(process.env.CLIENT_SECRET);
+console.log(process.env.CALENDAR_ID);
+
 const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
@@ -15,7 +20,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://coleaiseosa.github.io/meet/"],
+  redirect_uris: ["https://coleaiseosa.github.io/meet"],
   javascript_origins: ["https://coleaiseosa.github.io", "http://localhost:3000"],
 };
 
@@ -41,4 +46,45 @@ module.exports.getAuthURL = async () => {
       authUrl: authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+  // the values used to instantiate the OAuth Client are at the top of the file
+  const oAuthClient = new google.auth.Oauth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // to decode authorization code extracted from the URL queryS
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+  .then((token) => {
+    // respond with OAuth token
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(token),
+    };
+  })
+  .catch((err) => {
+    console.error(err);
+    return{
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(err),
+    };
+  });
 };
